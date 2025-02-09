a6bf5aecdf3c6c00c20a5a5f450519a50403e969
Julien-FRA <julien.francois@hetic.net>
2025-02-09 16:21:41 +0100

    <feat>(data): mock des nouvelles data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,14 @@
-"use client"
+"use client";
 
-import InitData from "@/components/molecules/InitData";
 import List from "@/components/organisms/Lists";
-import { ListsType } from "@/types/lists";
+import { fetchLocalStorage } from "@/helpers/apiLayers";
 
 export default function Home() {
-  const listOne: ListsType = { 
-    title: 'My first list', 
-    cards: [{
-      title: 'My first card', followed: false
-    },
-    {
-      title: 'My second card', followed: false
-
-    },
-    {
-      title: 'Followed card', followed: true
-
-    }]
-  };
-
-  const listTwo: ListsType = { 
-    title: 'My second list', 
-    cards: [{
-      title: 'Followed card with desc', desc: 'This is the description for card 1', followed: true},
-    ]
-  };
+  const boardListData = fetchLocalStorage();
 
   return (
-      <main className="main">
-        <InitData />
-        <List lists={[listOne, listTwo]} />
-      </main>
+    <main className="main">
+      <List board={boardListData} />
+    </main>
   );
 }
