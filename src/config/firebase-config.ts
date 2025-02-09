f248064dedd8dc2fd2510b72396f9b27bb4dba92
Julien-FRA <julien.francois@hetic.net>
2025-02-09 18:02:59 +0100

    <feat>(deploy): Deploy firebase

---
+++ src/config/firebase-config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2N-KsZrwi5a4MJfdNHdA8Wfxi92XfwEI",
+  authDomain: "trello-app-8bb80.firebaseapp.com",
+  projectId: "trello-app-8bb80",
+  storageBucket: "trello-app-8bb80.firebasestorage.app",
+  messagingSenderId: "1055635388052",
+  appId: "1:1055635388052:web:2a1ac89023a64fe8482681",
+  measurementId: "G-BDZFLD56SS",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
